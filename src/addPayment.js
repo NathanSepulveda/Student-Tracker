6649f8383e4eabcedae25bb94b9f85d4a42b2039
nathan sepulveda <nathans.composer@gmail.com>
2019-02-05 13:35:49 -0600

    payments are now working as well

--- src/addPayment.js
+++ src/addPayment.js
@@ -5,39 +5,43 @@ studentAPI.getPaymentMethods()
         console.log(methods)
         methods.forEach(method => {
             let html = `
-    <option value=${method.method}>${method.method}</option>
+    <option value=${method.id}>${method.method}</option>
     `
             paymentDropdown.innerHTML += html
         })
     })
-// document.querySelector("#recordLessonNotes").addEventListener("click", event => {
-//     let paymentsUrl = `http://localhost:3000/students`
-//     let title = document.querySelector("#notesTitle")
-//     let studentName = document.querySelector("#people").value
-//     // title.innerHTML = studentName
-//     let studentId = document.querySelector(".studentDisplay").id
-//     // let studentId = document.querySelector("#people").id
-//     console.log(studentName)
-//     let numericalStudentId = parseInt(studentId)
-//     let paymentDate = document.querySelector("#paymentDate").value
-//     let lessonNote = document.querySelector("#notesInput").value
-//     // let studentId = studentThing.className
-//     console.log(studentId)
-//     let fullNote = 
-//     {
-//         studentId: numericalStudentId,
-//         date: lessonDate,
-//         note: lessonNote
-//       }
-//     console.log(studentId)
-//     fetch(`${notesUrl}/${studentId}/notes`, {
-//     method: 'POST', // or 'PUT'
-//     body: JSON.stringify(fullNote), // data can be `string` or {object}!
-//     headers: {
-//         'Content-Type': 'application/json'
-//     }
-// }).then(res => res.json())
-//     .then(response => console.log('Success:', JSON.stringify(response)))
-//     .catch(error => console.error('Error:', error));
+document.querySelector("#recordStudentPayment").addEventListener("click", event => {
+    let paymentsUrl = `http://localhost:3000/students`
+    // let title = document.querySelector("#notesTitle")
+    let studentName = document.querySelector("#people").value
+    // title.innerHTML = studentName
+    let studentId = document.querySelector(".studentDisplay").id
+    // let studentId = document.querySelector("#people").id
+    console.log(studentName)
+    let numericalStudentId = parseInt(studentId)
+    let paymentDate = document.querySelector("#paymentDate").value
+    let paymentAmount = document.querySelector("#paymentAmount").value
+    let lessonNote = document.querySelector("#notesInput").value
+    let paymentMethodId = document.querySelector("#paymentMethod").value
+    // let studentId = studentThing.className
+    console.log(studentId)
+    let fullPayment =
+    {
+        studentId: studentId,
+        parentId: null,
+        date: paymentDate,
+        amount: paymentAmount,
+        methodId: paymentMethodId
+    }
+    console.log(studentId)
+    fetch(`${paymentsUrl}/${studentId}/payments`, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(fullPayment), // data can be `string` or {object}!
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
 
-// })
+})
